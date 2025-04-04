44e47e188012ab8462625ad27be810c975dd9a94
Katleho Mofokeng <katlehom814@gmail.com>
2025-04-04 15:25:06 +0200

    Updated basic.R

--- basic.R
+++ basic.R
@@ -2,9 +2,16 @@
 # "The simple graph has brought more information to the data analyst’s mind than any other device."-John Tukey
 # "Every dataset should be tidy (organized), and every operation should be human-readable."
 
-#Quick plot
 
-library(ggplot2)
-p <- ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width))+geom_point(color = "blue")
+library(tidyverse)
 
+# Print starwars data
+cat("--- Star Wars Characters (First 3) ---\n")
+print(starwars[1:3, c("name", "height", "species")])
+
+# Plot
+cat("\n--- Plotting Height vs. Mass ---\n")
+p <- ggplot(starwars, aes(height, mass)) + 
+  geom_point(color = "blue") +
+  labs(title = "Star Wars: Size Stats")
 print(p)

03b5b53a8a77d8eb3c2efb7008da44f8584e9691
Katleho Mofokeng <katlehom814@gmail.com>
2025-04-04 13:18:41 +0200

    Variables

--- basic.R
+++ basic.R
@@ -1,2 +1,9 @@
 
-print("Hello, github from R!")
+# "The simple graph has brought more information to the data analyst’s mind than any other device."-John Tukey
+
+#Variables
+x <- 10
+y <- 20
+sum <- (x+y)
+
+print(paste("Sum is:",sum))
